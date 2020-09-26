893f5230f653e97d2569ed5fd9d273002af6dbb8
nethmi-1997 <63659987+nethmi-1997@users.noreply.github.com>
2020-09-27 00:32:50 +0530

    Nethmi second

--- app/src/main/java/com/courierx/courierx/Location.java
+++ app/src/main/java/com/courierx/courierx/Location.java
@@ -8,12 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class SetDelivery extends Fragment {
+
+public class Location extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_set_delivery, container, false);
+        return inflater.inflate(R.layout.fragment_location, container, false);
     }
 }
