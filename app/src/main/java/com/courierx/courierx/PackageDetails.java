fd49554cde34873d37bc8622f73e2bdc6d04ccac
AkilaRanasinghe <akilaranasingha41@gmail.com>
2020-09-27 12:53:09 +0530

    Second Commit

--- app/src/main/java/com/courierx/courierx/PackageDetails.java
+++ app/src/main/java/com/courierx/courierx/PackageDetails.java
@@ -6,17 +6,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class PackageDetails extends Fragment {
 
     EditText description, weight, sheduledDate;
     Button button2;
+    TextView packageId;
+    CheckBox fragile;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_package_details, container, false);
+        View view = inflater.inflate(R.layout.fragment_package_details, container, false);
+        packageId = view.findViewById(R.id.packageID);
+        description = view.findViewById(R.id.description);
+        weight = view.findViewById(R.id.weight);
+        sheduledDate = view.findViewById(R.id.sheduleDate);
+        fragile = view.findViewById(R.id.fragile);
+        return view;
     }
 }
