0fd3986fbfbed27563b28713fecb21f84ef273c9
harshani <it19025940@my.sliit.lk>
2020-09-27 14:04:39 +0530

    Added CreditLog class

---
+++ app/src/main/java/com/courierx/courierx/CreditLog.java
@@ -0,0 +1,28 @@
+package com.courierx.courierx;
+
+public class CreditLog {
+
+    String date;
+    float amount;
+
+    public CreditLog(String date, float amount) {
+        this.date = date;
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+}
