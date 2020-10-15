991396196e93279ade8ab2a4fa4f678c2b8baa5d
AkilaRanasinghe <akilaranasingha41@gmail.com>
2020-10-16 01:20:29 +0530

    Anthima Last Commit

--- app/src/main/java/com/courierx/courierx/Track/TrackedAndPickedDelivery.java
+++ app/src/main/java/com/courierx/courierx/Track/TrackedAndPickedDelivery.java
@@ -85,7 +85,7 @@ public class TrackedAndPickedDelivery extends Fragment {
                     sndrcvrtxt.setText("Sender");
                     sender.setText(sndr);
                 }
-                if ((status.equals("Pending") && sndr.equals(person)) || (status.equals("Delivered") && sndr.equals(person))){
+                if ((status.equals("Pending") && sndr.equals(person)) || status.equals("Delivered")){
                     delt.setVisibility(View.VISIBLE);
                 }
                 if (status.equals("Pending") && sndr.equals(person)){
