58bc76e43f227df8c60235ce5b2415be3c40a442
Harshi-Dole <harshanidole1@gmail.com>
2020-10-16 22:25:08 +0530

    Implementation of delete

--- app/src/main/java/com/courierx/courierx/CreditLogViewHolder.java
+++ app/src/main/java/com/courierx/courierx/CreditLogViewHolder.java
@@ -1,6 +1,7 @@
 package com.courierx.courierx;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 public class CreditLogViewHolder extends RecyclerView.ViewHolder {
 
     TextView amount,date,type;
+    ImageView deleteBtn;
 
 
     public CreditLogViewHolder(@NonNull View itemView) {
@@ -16,5 +18,6 @@ public class CreditLogViewHolder extends RecyclerView.ViewHolder {
         amount = itemView.findViewById(R.id.credit_log_amount);
         date = itemView.findViewById(R.id.credit_log_date);
         type=itemView.findViewById(R.id.credit_log_type);
+        deleteBtn = itemView.findViewById(R.id.credit_log_delete_btn);
     }
 }
